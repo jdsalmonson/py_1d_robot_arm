f19dc35daf91bef431c6f05b70274f815009eaa7
Jay Salmonson <salmonson1@llnl.gov>
2022-12-31 00:11:47 -0800

    clean up animation

--- animate_1D_robot.py
+++ animate_1D_robot.py
@@ -24,25 +24,7 @@ odf = OneDFinger(f_kinetic=f_kinetic, v_o_atol=f_kinetic * 2.0e-2)
 
 t, y = odf.integrate()
 
-"""
-plt.axes()  # [0, 20, 0, 20])
-plt.grid(True)
-
-circle = plt.Circle((0, 0), 1.5, fc="blue", ec="red")
-e = Ellipse((0, 0), 1.0, 0.5)
-rect = Rectangle(
-    (1, -0.5),
-    0.5,
-    1.0,
-    color="green",
-)
-ax = plt.gca()
-ax.add_patch(e)  # circle)  # e)
-ax.add_patch(rect)
-"""
-
-# plt.axis("equal")  # scaled")
-
+# Initialize the figure
 n_frames = 100
 interp_pos = interp1d(t, y[:2, :])
 times = np.linspace(0, 10, n_frames)
@@ -50,21 +32,12 @@ x = interp_pos(times)
 x_f = x[0, :]
 x_o = x[1, :]
 
-# plt.plot(t, y.T)
-# plt.plot(t, odf.F_a(t), "--")
-# plt.plot(t, odf.F_sensor_func(t, y), "-")
-
-# fig = plt.figure()
-# ax = plt.gca()
-
 fig, axs = plt.subplots(3, 1, figsize=(8, 8))
 axa = axs[0]
 axp = axs[1]
 axf = axs[2]
 
 patches = []
-# initialize an empty list of cirlces
-# x_f, x_o = interp_pos(0.0)
 e = Ellipse((x_f[0], 0), 1.0, 0.5, color="blue")
 rect = Rectangle(
     (x_o[0], -0.5),
@@ -72,8 +45,8 @@ rect = Rectangle(
     1.0,
     color="green",
 )
-patches.append(e)  # ax.add_patch(e))  # circle)
-patches.append(rect)  # ax.add_patch(rect))
+patches.append(e)
+patches.append(rect)
 
 collection = clt.PatchCollection(patches)
 axa.add_collection(collection)
@@ -92,37 +65,8 @@ axf.legend(loc="upper right")
 axf.set_xlabel("time (s)")
 vlf = axf.axvline(times[0], color="red")
 
-""" 
-def init():
-    ax.set_xlim((-1, 5))
-    ax.set_ylim((-1, 1))
-
-    # return []
-    patches = []
-
-    # initialize an empty list of cirlces
-    e = Ellipse((x_f[0], 0), 1.0, 0.5, color="blue")
-    rect = Rectangle(
-        (x_o[0], -0.5),
-        0.5,
-        1.0,
-        color="green",
-    )
-
-    # ax = plt.gca()
-    patches.append(e)  # ax.add_patch(e))  # circle)
-    patches.append(rect)  # ax.add_patch(rect))
-    collection = clt.PatchCollection(patches)
-    ax.add_collection(collection)
-
-    return collection
-
-    # return patches
- """
-
 
 def animate(i):
-    # draw circles, select to color for the circles based on the input argument i.
 
     patches = []
     e = Ellipse((x_f[i], 0), 1.0, 0.5)
@@ -133,16 +77,13 @@ def animate(i):
         color="green",
     )
 
-    # ax = plt.gca()
-    patches.append(e)  # ax.add_patch(e))  # circle)
-    patches.append(rect)  # ax.add_patch(rect))
+    patches.append(e)
+    patches.append(rect)
     collection.set_paths(patches)
     collection.set_facecolors(["blue", "green"])
 
     vl.set_xdata([times[i], times[i]])
     vlf.set_xdata([times[i], times[i]])
-
-    # return patches  # collection
 
 
 anim = animation.FuncAnimation(
@@ -151,8 +92,7 @@ anim = animation.FuncAnimation(
     # init_func=init,
     frames=n_frames,
     interval=20,
-    blit=False,  # True
+    blit=False,
 )
 
-# plt.legend()
 plt.show()
