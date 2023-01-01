3ac4cea85dab73b87385cff73182275c82857fef
Jay Salmonson <salmonson1@llnl.gov>
2022-12-31 19:43:15 -0800

    animate html, gif added. first README

---
+++ README.md
@@ -0,0 +1,8 @@
+[//]: <> (shift-command-V to toggle preview)
+OneD Robot
+==========
+
+This is an implementation of a 1D Robot.  It is comprised of only a finger that moves back and forth according to an applied force.  The finger tip interacts with an object; touching, pushing or bouncing off of it.  A force sensor on the finger tip records the touch interaction with the object.  As such this is one of the simplest robot simulations of which one can conceive.
+
+![](/images/oneD_robot.gif)
+
--- animate_1D_robot.py
+++ animate_1D_robot.py
@@ -32,13 +32,19 @@ x = interp_pos(times)
 x_f = x[0, :]
 x_o = x[1, :]
 
+x_min = np.min(x)
+x_max = np.max(x)
+
+interp_f_sensor = interp1d(t, odf.F_sensor_func(t, y))
+
 fig, axs = plt.subplots(3, 1, figsize=(8, 8))
 axa = axs[0]
 axp = axs[1]
 axf = axs[2]
 
 patches = []
-e = Ellipse((x_f[0], 0), 1.0, 0.5, color="blue")
+e_x = 1.0
+e = Ellipse((x_f[0] - 0.5 * e_x, 0.0), e_x, 0.5, color="blue")
 rect = Rectangle(
     (x_o[0], -0.5),
     0.5,
@@ -50,26 +56,34 @@ patches.append(rect)
 
 collection = clt.PatchCollection(patches)
 axa.add_collection(collection)
-axa.set_xlim((-1, 5))
+axa.set_xlim((x_min - 0.5 - e_x, x_max + 0.5))
 axa.set_ylim((-1, 1))
 axa.set_aspect("equal")
+axa.get_yaxis().set_visible(False)
 
-axp.plot(t, y[:2, :].T)
+# position plot
+axp.plot(t, y[0, :].T, color="blue")
+axp.plot(t, y[1, :].T, color="green")
 vl = axp.axvline(times[0], color="red")
+(xfp,) = axp.plot(times[0], x_f[0], "o", color="blue")
+(xop,) = axp.plot(times[0], x_o[0], "o", color="green")
 axp.grid(True)
 
-axf.plot(t, odf.F_a(t), "--", label="F_applied")
-axf.plot(t, odf.F_sensor_func(t, y), "-", color="magenta", label="F_sensor")
+# force plot
+axf.plot(t, interp_f_sensor(t), "-", color="magenta", label="F_sensor")
+axf.plot(t, odf.F_a(t), "--", color="cyan", label="F_applied")
+vlf = axf.axvline(times[0], color="red")
+(fas,) = axf.plot(times[0], interp_f_sensor(times[0]), "o", color="magenta")
+(fap,) = axf.plot(times[0], odf.F_a(times[0]), "o", color="cyan")
 axf.grid(True)
 axf.legend(loc="upper right")
 axf.set_xlabel("time (s)")
-vlf = axf.axvline(times[0], color="red")
 
 
 def animate(i):
 
     patches = []
-    e = Ellipse((x_f[i], 0), 1.0, 0.5)
+    e = Ellipse((x_f[i] - 0.5 * e_x, 0), e_x, 0.5)
     rect = Rectangle(
         (x_o[i], -0.5),
         0.5,
@@ -82,8 +96,14 @@ def animate(i):
     collection.set_paths(patches)
     collection.set_facecolors(["blue", "green"])
 
+    # update the position plot
     vl.set_xdata([times[i], times[i]])
+    xfp.set_data(times[i], x_f[i])
+    xop.set_data(times[i], x_o[i])
+    # update the force plot
     vlf.set_xdata([times[i], times[i]])
+    fas.set_data(times[i], interp_f_sensor(times[i]))
+    fap.set_data(times[i], odf.F_a(times[i]))
 
 
 anim = animation.FuncAnimation(
@@ -95,4 +115,25 @@ anim = animation.FuncAnimation(
     blit=False,
 )
 
-plt.show()
+save_as = "gif"  # "html"  # "gif"
+if save_as == "html":
+    # Make HTML output: -------------------
+    from matplotlib.animation import HTMLWriter
+    import matplotlib
+
+    # Increase size limit for html file:
+
+    matplotlib.rcParams["animation.embed_limit"] = 2**32  # 128
+    anim.save("oneD_robot.html", writer=HTMLWriter(embed_frames=True))
+
+    # To open file in web browser:
+    # > xdg-open symmetric_pendulum.html
+    # --------------------------------------
+elif save_as == "gif":
+    # Make GIF output: -------------------
+    from matplotlib.animation import PillowWriter
+
+    anim.save("oneD_robot.gif", writer=PillowWriter(fps=30))
+    # --------------------------------------
+else:
+    plt.show()
